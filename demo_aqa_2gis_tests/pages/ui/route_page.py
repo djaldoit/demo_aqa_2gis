2ab4262b0f42f34f54562b4cd68dcd0b74ed34d7
Dzhalal Atabaev <djaldoit@gmail.com>
2024-07-03 14:43:21 +0300

    correction of comments 0.6

--- demo_aqa_2gis_tests/pages/ui/route_page.py
+++ demo_aqa_2gis_tests/pages/ui/route_page.py
@@ -11,8 +11,8 @@ class RoutePage:
 
     @allure.step('Поиск маршрута')
     def searching_for_route(self):
-        browser.element('//input[@placeholder="Поиск в 2ГИС"]').type('Московский Кремль').press_enter().press_enter()
-        browser.element('//input[@placeholder="Поиск в 2ГИС"]').type('Московский Кремль').press_enter()
+        browser.element('//input[@placeholder="Поиск в 2ГИС"]').should(be.visible).type(
+            'Московский Кремль').press_enter()
 
     @allure.step('Клик по маршруту')
     def finding_route(self):
