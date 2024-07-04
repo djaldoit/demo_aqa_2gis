236a9fade556bcd7529813300862a4be9b60fe64
Dzhalal Atabaev <djaldoit@gmail.com>
2024-07-04 13:43:25 +0300

    correction of comments 1.8

--- tests/test_pages_os_app.py
+++ tests/test_pages_os_app.py
@@ -4,7 +4,7 @@ from demo_aqa_2gis_tests.pages.device_page import device_page
 
 
 @pytest.mark.parametrize('device', ['Android', 'iOS', 'ПК-версия'])
-@allure.title(lambda device: f'Проверка сайта приложений для {device}')
+@allure.title('Проверка сайта приложений для операционных устройств')
 def test_os_app(device):
     device_page.browser_open()
     device_page.click_menu()
