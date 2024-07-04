04623010cdd9ada70b4727a2ffb05b3a57a5fc82
Dzhalal Atabaev <djaldoit@gmail.com>
2024-07-04 19:22:52 +0300

    correction of comments 2.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from dotenv import load_dotenv
 from selene import browser
 from selenium.webdriver.chrome.options import Options
+
+from demo_aqa_2gis_tests.pages.log_in_page import login_page
 from demo_aqa_2gis_tests.utils import attachments
 from tests.test_log_in import test_checking_account_log_in
 
@@ -23,7 +25,12 @@ def load_env():
 
 @pytest.fixture
 def account_login():
-    return test_checking_account_log_in()
+    login_page.browser_open()
+    login_page.click_on_log_in()
+    login_page.click_on_email_sign_in()
+    login_page.login_in()
+    login_page.confirm_that_we_are_logged_in()
+    login_page.checking_that_we_are_logging_in()
 
 
 @pytest.fixture(scope='function', autouse=True)
