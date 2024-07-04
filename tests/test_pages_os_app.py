32a8e713897a4e146092c635d55b18e5f2826c29
Dzhalal Atabaev <djaldoit@gmail.com>
2024-07-04 12:26:50 +0300

    correction of comments 1.1

--- tests/test_pages_os_app.py
+++ tests/test_pages_os_app.py
@@ -3,17 +3,17 @@ import pytest
 from demo_aqa_2gis_tests.pages.device_page import device_page
 
 
-@pytest.mark.parametrize('device', ['android', 'ios', 'pc'])
+@pytest.mark.parametrize('device', ['Android', 'iOS', 'ПК-версия'])
 @allure.title('Проверка сайта приложений для Android, iOS и ПК устройств')
-def test_device_version(device):
+def test_os_app(device):
     device_page.browser_open()
     device_page.click_on_the_menu()
-    if device == 'android':
+    if device == 'Android':
         device_page.click_on_android()
         device_page.should_android_page()
-    elif device == 'ios':
+    elif device == 'iOS':
         device_page.click_on_ios()
         device_page.should_ios_page()
-    elif device == 'pc':
+    elif device == 'ПК-версия':
         device_page.click_on_pc()
         device_page.should_pc_page()
